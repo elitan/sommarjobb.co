715a32e0479ecd942060c1b959bebfef5e67323f
Johan Eliasson <johan@eliasson.me>
2021-11-21 18:24:10 +0100

    started

--- scripts/locations/locations.js
+++ scripts/locations/locations.js
@@ -1,12 +1,27 @@
 import { NhostClient } from "@nhost/nhost-js";
 import fs from "fs";
-import slugify from "slugify";
 
 const nhost = new NhostClient({
   backendUrl: "https://kqgngjmtygqovuixntdc.nhost.run",
 });
 
-fs.readFile("locations.json", "utf8", (err, data) => {
+const INSERT_REGION = `
+mutation insertRegions($regions: [regions_insert_input!]!) {
+  insertRegions(objects:$regions) {
+    affected_rows
+  }
+}
+`;
+
+function slugify(s) {
+  return s.replace(" ", "-").toLowerCase();
+}
+
+let mun = new Set();
+
+let regions = [];
+
+fs.readFile("locations.json", "utf8", async (err, data) => {
   if (err) {
     console.error(err);
     return;
@@ -17,7 +32,53 @@ fs.readFile("locations.json", "utf8", (err, data) => {
 
   data.forEach((region) => {
     console.log({ region });
+    console.log(JSON.stringify(region, null, 2));
     console.log(region.name);
     console.log(slugify(region.name, { lower: true }));
+
+    const municipalities = {
+      data: [],
+    };
+
+    region.items.forEach((item) => {
+      const nameSlug = slugify(item.name);
+
+      if (mun.has(nameSlug)) {
+        console.log("already there!");
+        console.log(item);
+        console.log(nameSlug);
+        process.exit(1);
+      }
+
+      mun.add(nameSlug);
+
+      municipalities.data.push({
+        slug: slugify(item.name),
+        name: item.name,
+        arbetsformedlingenId: item.id,
+      });
+    });
+
+    regions.push({
+      slug: slugify(region.name),
+      name: region.name,
+      arbetsformedlingenId: region.id,
+      municipalities,
+    });
   });
+
+  console.log(JSON.stringify(regions, null, 2));
+
+  let { data: gqlData, error } = await nhost.graphql.request(
+    INSERT_REGION,
+    { regions },
+    {
+      headers: {
+        "x-hasura-admin-secret": "481bdb347655346a426e1a1dc9c805e1",
+      },
+    }
+  );
+
+  console.log({ error });
+  console.log({ gqlData });
 });
